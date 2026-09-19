b483d5b9747bc568c8bf48b3037b8ee8cd58b6b2
shotaphutkaradze <shotafutk09@icloud.com>
2026-09-19 14:10:31 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const data = document.createElement('ul');
+for(i=0; i<10; i++){
+    const lint = document.createElement("li");
+    data.append(lint);
+    lint.textContent = Math.floor(Math.random()*9+1);
+}
+const newElement = (dataInfo) => {
+   const element = document.createElement('li');
+   element.textContent = dataInfo;
+   data.append(element); 
+}
+document.body.append(data);
+const clicker = document.querySelector('.clicker');
+const clickerInput = document.querySelector('.clickerInput');
+clicker.addEventListener("click", () => {
+    newElement(clickerInput.value);
+});
